31fa2033c8fbf225d531953f24cbf037f3cac658
dvir <dmenajem@gmail.com>
2021-08-22 10:36:56 +0300

    add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { LayoutModule } from '@angular/cdk/layout';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from 'src/material/material.module';
 import { AppComponent } from './components/root/app.component';
+import { IconComponent } from './components/icon/icon.component';
+import { FormContainerComponent } from './components/form/form-container/form-container.component';
+import { FormInputComponent } from './components/form/form-input/form-input.component';
+import { InputRadioComponent } from './components/form/input-radio/input-radio.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+    FormContainerComponent,
+    FormInputComponent,
+    InputRadioComponent,
+    IconComponent,
+  ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MaterialModule,
     FormsModule,
     ReactiveFormsModule,
+    LayoutModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
