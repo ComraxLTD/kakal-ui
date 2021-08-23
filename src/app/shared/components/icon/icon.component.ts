ecc149df1913f92f3f71e128981663522de73ba8
dvir <dmenajem@gmail.com>
2021-08-23 08:51:01 +0300

    update modules

---
+++ src/app/shared/components/icon/icon.component.ts
@@ -0,0 +1,24 @@
+import { IconsService } from '../../../utilities/icons/icons.service';
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-icon',
+  templateUrl: './icon.component.html',
+  styleUrls: ['./icon.component.scss']
+})
+export class IconComponent implements OnInit {
+
+  @Input() public type: string = "svg"
+  @Input() public isActive: boolean = false
+  @Input() public key: string = ""
+  @Input() public color: string = "primary"
+
+  constructor(
+    private iconsService: IconsService
+  ) { }
+
+  ngOnInit(): void {
+    this.iconsService.setIcon(this.key)
+  }
+
+}
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { StatusCardComponent } from './components/status-card/status-card.component';
+import { TypographyComponent } from './components/typography/typography.component';
+import { IconComponent } from './components/icon/icon.component';
+import { MaterialModule } from 'src/material/material.module';
+import { IconCardComponent } from './components/icon-card/icon-card.component';
+
+
+
+@NgModule({
+  declarations: [
+    StatusCardComponent,
+    TypographyComponent,
+    IconComponent,
+    IconCardComponent
+
+  ],
+  imports: [
+    CommonModule,
+    MaterialModule
+  ], exports : [
+    StatusCardComponent,
+    TypographyComponent,
+    IconComponent,
+    IconCardComponent
+  ]
+})
+export class SharedModule { }
