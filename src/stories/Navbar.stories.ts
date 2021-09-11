ffe1964fa6306074dc5b7b5d952ff34415dabce6
matan-mac <matanby94@gmail.com>
2021-09-12 02:14:38 +0300

    feat: Added navbar story

---
+++ src/stories/Navbar.stories.ts
@@ -0,0 +1,25 @@
+import {Meta, moduleMetadata, Story} from '@storybook/angular';
+import {APP_BASE_HREF, CommonModule} from '@angular/common';
+import {NavbarComponent} from "../app/components/navigation/navbar/navbar.component";
+import {MaterialModule} from "../material/material.module";
+import {BrowserModule} from "@angular/platform-browser";
+import {AppRoutingModule} from "../app/app-routing.module";
+
+export default {
+  title: 'Nav Bar',
+  component: NavbarComponent,
+  decorators: [
+    moduleMetadata({
+      declarations: [],
+      providers: [{provide: APP_BASE_HREF, useValue: '/'}],
+      imports: [CommonModule, MaterialModule, BrowserModule, AppRoutingModule],
+    }),
+  ],
+} as Meta;
+
+const Template: Story<NavbarComponent> = (args: NavbarComponent) => ({
+  props: args,
+});
+
+export const Default = Template.bind({});
+Default.args = {};
