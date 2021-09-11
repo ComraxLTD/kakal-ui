d61808a9ce3ada272105a1bb4927ca278a18934b
matan-mac <matanby94@gmail.com>
2021-09-12 02:33:27 +0300

    fix: Changed navbar to the to-nav component

--- src/stories/Navbar.stories.ts
+++ src/stories/Navbar.stories.ts
@@ -1,26 +1,49 @@
 import {Meta, moduleMetadata, Story} from '@storybook/angular';
 import {APP_BASE_HREF, CommonModule} from '@angular/common';
-import {NavbarComponent} from "../app/components/navigation/navbar/navbar.component";
 import {MaterialModule} from "../material/material.module";
 import {BrowserModule} from "@angular/platform-browser";
 import {AppRoutingModule} from "../app/app-routing.module";
 import {MatToolbarModule} from "@angular/material/toolbar";
+import {ToNavComponent} from "../app/components/navigation/to-nav/to-nav.component";
+import {StatusCardComponent} from "../app/components/cards/status-card/status-card.component";
+import {IconComponent} from "../app/components/icon/icon.component";
 
 export default {
   title: 'Nav Bar',
-  component: NavbarComponent,
+  component: ToNavComponent,
   decorators: [
     moduleMetadata({
-      declarations: [],
+      declarations: [StatusCardComponent, IconComponent],
       providers: [{provide: APP_BASE_HREF, useValue: '/'}],
       imports: [CommonModule, MaterialModule, MatToolbarModule, BrowserModule, AppRoutingModule],
     }),
   ],
 } as Meta;
 
-const Template: Story<NavbarComponent> = (args: NavbarComponent) => ({
+const Template: Story<ToNavComponent> = (args: ToNavComponent) => ({
   props: args,
 });
 
 export const Default = Template.bind({});
-Default.args = {};
+Default.args = {
+  status: [
+    {
+      title: 'בתהליך',
+      svgUrl: 'reload',
+      badgeValue: 3,
+      hasBadge: true
+    },
+    {
+      title: 'מחכה לאישור',
+      svgUrl: 'report',
+      badgeValue: 1,
+      hasBadge: true
+    },
+    {
+      title: 'סגור',
+      svgUrl: 'flag',
+      badgeValue: 20,
+      hasBadge: true
+    },
+  ],
+};
