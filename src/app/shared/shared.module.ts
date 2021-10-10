113676ec2a8ffeb027e3f62e37a665c79ad77632
dvir <dmenajem@gmail.com>
2021-10-10 09:15:25 +0300

    feat (shared.modeule) - remove unuse comps

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,23 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { StatusCardComponent } from './components/status-card/status-card.component';
-import { TypographyComponent } from './components/typography/typography.component';
 import { MaterialModule } from 'src/material/material.module';
 
-
-
 @NgModule({
-  declarations: [
-    StatusCardComponent,
-    TypographyComponent,
-
-  ],
-  imports: [
-    CommonModule,
-    MaterialModule
-  ], exports : [
-    StatusCardComponent,
-    TypographyComponent,
-  ]
+  declarations: [],
+  imports: [CommonModule, MaterialModule],
+  exports: [],
 })
-export class SharedModule { }
+export class SharedModule {}
