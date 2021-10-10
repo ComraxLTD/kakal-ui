38165eca5840fbc6cd17beac606b64d1f73d6790
dvir <dmenajem@gmail.com>
2021-10-10 16:12:44 +0300

    feat (size.directive) - update with breakpoint service

--- src/app/utilities/directives/size.directive.ts
+++ src/app/utilities/directives/size.directive.ts
@@ -1,5 +1,7 @@
 import { Directive, HostBinding, Input } from '@angular/core';
+import { Observable, Subscription } from 'rxjs';
 import { StepType } from '../../components/step/step.model';
+import { BreakpointService } from '../services/breakpoint.service';
 
 @Directive({
   selector: '[appSize]',
@@ -7,30 +9,50 @@ import { StepType } from '../../components/step/step.model';
 export class SizeDirective {
   @Input() size: number;
   @Input() type: StepType;
-  @Input() divider: number ;
+  @Input() divider: number;
+  private tablet$: Observable<boolean>;
+
+  private subscription: Subscription;
 
   @HostBinding('style.height') public height: string;
   @HostBinding('style.width') public width: string;
 
-  constructor() { }
+  constructor(private breakpointService: BreakpointService) {}
 
-  ngOnInit() {
+
+  ngOnInit(): void {
+    this.setSize();
+    this.tablet$ = this.breakpointService.isTablet();
+    this.subscribeToBreakpoint();
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+
+  private setSize() {
     switch (this.type) {
       case 'wizard':
         this.width = '6rem';
         this.height = '7.5rem';
         break;
       case 'status':
-        this.width = `${this.size * 3}rem`;
-        this.height = `${this.size}rem`;
+        this.width = `6rem`;
+        this.height = `6rem`;
         break;
       case 'step':
-        this.width = `7rem`;
-        this.height = `7rem`;
+        this.width = `8rem`;
+        this.height = `12rem`;
         break;
       default:
         this.width = `${this.size * (this.divider || 1)}rem`;
         this.height = `${this.size}rem`;
     }
+  }
+
+  private subscribeToBreakpoint() {
+    this.subscription = this.tablet$.subscribe((tablet: boolean) => {
+      this.setSize();
+    });
   }
 }
