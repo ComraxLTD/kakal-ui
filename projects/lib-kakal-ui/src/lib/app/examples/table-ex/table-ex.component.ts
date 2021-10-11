5080aa4bd9abf7a142f2039fa10584d1262a4514
dvir <dmenajem@gmail.com>
2021-10-11 15:02:52 +0300

    feat (inner-files) -

--- projects/lib-kakal-ui/src/lib/app/examples/table-ex/table-ex.component.ts
+++ projects/lib-kakal-ui/src/lib/app/examples/table-ex/table-ex.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { ThemePalette } from '@angular/material/core';
-import { ColumnModel } from 'projects/lib-kakal-ui/src/lib/app/components/columns/column.model';
+import { ColumnModel } from '../../components/columns/column.model';
 import { TableOptions } from '../../components/table/table.component';
 
 @Component({
