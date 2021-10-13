21fcddd704ffb45e9e374cdb420c4f3c8dc9dd9c
DvirMon <dmenajem@gmail.com>
2021-10-13 15:30:45 +0300

    feat- (menu + expand-panel) - updae menu and expand-panel

--- projects/lib-kakal-ui/src/lib/stories/Menu.stories.ts
+++ projects/lib-kakal-ui/src/lib/stories/Menu.stories.ts
@@ -13,20 +13,17 @@ import { IconComponent } from '../app/components/icon/icon.component';
 import { ClassesDirective } from '../app/utilities/directives/classes.directive';
 import { ColorDirective } from '../app/utilities/directives/color.directive';
 import { SizeDirective } from '../app/utilities/directives/size.directive';
-
-import { MenuService } from '../app/components/menu/menu.service';
-import { RouterService } from '../app/utilities/services/route.rservice';
+import { ButtonDirective } from '../app/utilities/directives/button.directive';
 
 export default {
   title: 'Menu',
   component: MenuComponent,
   decorators: [
     moduleMetadata({
-      declarations: [MenuItemComponent, CardUserComponent, TypographyComponent, IconComponent, SizeDirective, ColorDirective, ClassesDirective],
-      providers: [MenuService, RouterService { provide: APP_BASE_HREF, useValue: '/' }],
+      declarations: [MenuItemComponent, CardUserComponent, TypographyComponent, IconComponent, SizeDirective, ColorDirective, ClassesDirective, ButtonDirective],
+      providers: [],
       imports: [
         CommonModule,
-        AppRoutingModule,
         MaterialModule,
         BrowserAnimationsModule,
       ],
