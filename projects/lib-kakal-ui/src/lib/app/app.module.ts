aca2ac48a3f4cbe9a03d474ba27ff706ba945aff
DvirMon <dmenajem@gmail.com>
2021-10-14 16:02:10 +0300

    feat- (layout)  - add layout screen

--- projects/lib-kakal-ui/src/lib/app/app.module.ts
+++ projects/lib-kakal-ui/src/lib/app/app.module.ts
@@ -60,11 +60,15 @@ import { FormatPipe } from './utilities/pipes/format.pipe';
 import { ColumnFilterComponent } from './components/columns/column-filter/column-filter.component';
 import { ColumnFormComponent } from './components/columns/column-form/column-form.component';
 import { StatusComponent } from './components/status/status.component';
+import { LayoutComponent } from './screens/layout/layout.component';
 
 @NgModule({
   declarations: [
+
     AppComponent,
     DashboardComponent,
+    LayoutComponent,
+
     IconComponent,
     TypographyComponent,
 
