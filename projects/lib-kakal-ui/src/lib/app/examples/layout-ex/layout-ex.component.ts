2a984f846ca8138b8e90c308cfe2e240bcff5483
dvirmon <dmenajem@gmail.com>
2021-10-27 10:16:32 +0300

    featt (run-time files)

--- projects/lib-kakal-ui/src/lib/app/examples/layout-ex/layout-ex.component.ts
+++ projects/lib-kakal-ui/src/lib/app/examples/layout-ex/layout-ex.component.ts
@@ -24,6 +24,7 @@ export class LayoutExComponent implements OnInit {
   @Input() public steps: CardStepModel[];
   @Input() public menu: MenuModel[];
 
+  // path subscription
   private pathSubscription: Subscription;
 
   // NAVBAR SECTION
