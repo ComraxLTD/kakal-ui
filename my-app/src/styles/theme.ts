ec0978d13a2a3e7cb78ab50e19c50b99a55866be
dvirmon <dmenajem@gmail.com>
2022-01-06 14:42:11 +0200

    feat  (styles) - add styles folder to storybook and app project

---
+++ my-app/src/styles/theme.ts
@@ -0,0 +1,32 @@
+import { ThemePalette } from '@angular/material/core';
+
+export declare type Theme = 'dark'
+
+export declare type Palette =
+  | ThemePalette
+  | 'paper'
+  | 'default'
+  | 'disable'
+  | 'disableButton'
+  | 'disableText'
+  | 'table'
+  | 'text'
+  | 'success'
+  | 'graylight';
+
+export type Color = { [key in Palette]: string };
+
+export const palette: Color = {
+  primary: '#01a7b4',
+  accent: '#37c56b',
+  warn: '#d83020',
+  paper: '#fff',
+  default: '#bababa',
+  disable: ' rgba(0, 0, 0, 0.26)',
+  disableButton: 'rgba(0, 0, 0, 0.12);',
+  disableText: ' rgba(0, 0, 0, 0.26)',  table: '#f9f9f9',
+  text: '#000000',
+  success: '#59a437',
+  graylight: '#e2e1e1',
+};
+
