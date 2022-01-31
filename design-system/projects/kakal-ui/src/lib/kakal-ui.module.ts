57a95ee7401241e184b05cf07f653cbdf830c9d2
Matan <matanby94@gmail.com>
2022-01-31 09:26:22 +0200

    fix: removed imports

--- design-system/projects/kakal-ui/src/lib/kakal-ui.module.ts
+++ design-system/projects/kakal-ui/src/lib/kakal-ui.module.ts
@@ -3,21 +3,9 @@ import { CommonModule } from '@angular/common';
 import { KakalUiComponent } from './kakal-ui.component';
 import { ButtonComponent } from './button/button.component';
 import { ButtonsetComponent } from './buttonset/buttonset.component';
-import { AreaPipe } from './pipes/area.pipe';
-import { FormatPipe } from './pipes/format.pipe';
-import { LocationPipe } from './pipes/location.pipe';
-import { PrefixPipe } from './pipes/prefix.pipe';
-import { ButtonDirective } from './directives/button.directive';
-import { CellDirective } from './directives/cell.directive';
-import { OutsideDirective } from './directives/outside.directive';
-import { WizardDirective } from './directives/wizard';
 
-const exportPipes = [AreaPipe, FormatPipe, LocationPipe, PrefixPipe];
+const exportPipes = [];
 const exportDirective = [
-  ButtonDirective,
-  CellDirective,
-  OutsideDirective,
-  WizardDirective,
 ];
 
 @NgModule({
