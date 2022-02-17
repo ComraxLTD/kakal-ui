1d6b88d6c481abf4e40bff0f7e89b1d1d3f04e89
alex-shvetsov-comrax <alex.s@comrax.com>
2022-02-17 10:38:33 +0200

    preview.head added

--- design-system/.storybook/preview.js
+++ design-system/.storybook/preview.js
@@ -1,4 +1,5 @@
 
+
 import '!style-loader!css-loader!sass-loader!./../projects/kakal-ui/src/styles/global.scss';
 import { setCompodocJson } from "@storybook/addon-docs/angular";
 import docJson from "../documentation.json";
