5d0ab4ad6e6c36c1860ce67c41e85d92866a4fb8
dvirmon <dmenajem@gmail.com>
2022-02-21 19:43:41 +0200

    feat (autocmplete.story) - add multi autocompleye story

--- design-system/stories/form-autocomplete.stories.ts
+++ design-system/stories/form-autocomplete.stories.ts
@@ -46,3 +46,18 @@ autoComplete.args = {
   formDataSource:new FormDataSource(),
   control: new FormControl(),
 };
+export const autocompleteMulti = Template.bind({});
+autoComplete.args = {
+  question: new QuestionAutocompleteModel({
+    key: 'autocomplete',
+    multi : true,
+    options: [
+      { value: '1', label: 'first' },
+      { value: '2', label: 'second' },
+      { value: '3', label: 'thierd' },
+      { value: '4', label: 'foruth' },
+    ],
+  }),
+  formDataSource:new FormDataSource(),
+  control: new FormControl(),
+};
