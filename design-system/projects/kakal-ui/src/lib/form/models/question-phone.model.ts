47c218fbef90409448a9d4ae2a891e1f882468ca
Victor Nikiforov <victor@comrax.com>
2022-02-23 09:00:58 +0200

    added phone & email question

---
+++ design-system/projects/kakal-ui/src/lib/form/models/question-phone.model.ts
@@ -0,0 +1,13 @@
+import { ValidationService } from "../services/validations.service";
+import { QuestionNumberModel } from "./question-number.model";
+
+
+export class QuestionPhoneModel extends QuestionNumberModel {
+    constructor(options) {
+        super(options);
+        this.icon = 'phone';
+        this.validations = [...options?.validations, ValidationService.regex('phone')];
+        this.controlType = 'cleave';
+        this.cleave = { blocks: [3, 3, 4], delimiter: '-' };
+    }
+}
