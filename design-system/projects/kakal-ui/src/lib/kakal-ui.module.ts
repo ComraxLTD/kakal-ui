0b0c3b7b60b5eb6640bde4dc346ec4b4a1a074b6
dvirmon <dmenajem@gmail.com>
2022-02-24 16:45:18 +0200

    feat ()  - remove un use code

--- design-system/projects/kakal-ui/src/lib/kakal-ui.module.ts
+++ design-system/projects/kakal-ui/src/lib/kakal-ui.module.ts
@@ -1,11 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { KakalUiComponent } from './kakal-ui.component';
-import { FormComponent } from './form/form/form.component';
-import { FormDateComponent } from './form/form-date/form-date.component';
-import { FormInputComponent } from './form/form-input/form-input.component';
-import { FormRadioComponent } from './form/form-radio/form-radio.component';
-import { FormCurrencyComponent } from './form/form-currency/form-currency.component';
 import { MaterialModule } from './angular-material/material.module';
 import { ReactiveFormsModule } from '@angular/forms';
 import { CoreModule } from './core-module/core.module';
@@ -15,29 +9,16 @@ import { KKLFormAutoCompleteModule } from './form/form-autocomplete/form-autocom
 import { KKLFormUploadModule } from './form/form-upload/form-upload.module';
 import { KKLIconModule } from './icon/icon.module';
 import { KKLTypographyModule } from './typography/typography.module';
-import { FormSelectComponent } from './form/form-select/form-select.component';
 import { KKLMenuItemModule } from './menu-item/menu-item.module';
-import { ListItemComponent } from './list-item/list-item.component';
 import { KKLMenuModule } from './menu/menu.module';
 import { KklTitleModule } from './kkl-title/kkl-title.module';
 import { KKLStepTitleModule } from './step-title/step-title.module';
-import { FormExampleComponent } from './examples/form-example/form-example.component';
 import { KKLDirectivesModule } from './directives/directives.module';
 import { KKLPipesModule } from '../pipes/pipes.module';
 
-const formComps = [
-  FormComponent,
-  FormDateComponent,
-  FormInputComponent,
-  FormRadioComponent,
-  FormCurrencyComponent,
-];
+
 @NgModule({
   declarations: [
-    ...formComps,
-    FormSelectComponent,
-    ListItemComponent,
-    FormExampleComponent,
   ],
   imports: [
     CoreModule,
@@ -52,7 +33,6 @@ const formComps = [
     KKLStepTitleModule,
   ],
   exports: [
-    ...formComps,
     KKLMenuItemModule,
     KKLButtonModule,
     KKLIconModule,
--- design-system/projects/kakal-ui/src/public-api.ts
+++ design-system/projects/kakal-ui/src/public-api.ts
@@ -2,8 +2,6 @@
  * Public API Surface of kakal-ui
  */
 
-export * from './lib/kakal-ui.service';
-export * from './lib/kakal-ui.component';
 export * from './lib/kakal-ui.module';
 
 export * from './lib/button/button.component';
