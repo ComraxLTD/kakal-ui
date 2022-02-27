c02632d37ad5839b0138ff433100d5886facc629
Matan <matanby94@gmail.com>
2022-02-28 00:33:59 +0200

    feat: Added heroku assets

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var path = require("path");
 var serveStatic = require("serve-static");
 
 app = express();
-app.use(serveStatic(__dirname + "/projects/lib-kakal-ui/storybook-static"));
+app.use(serveStatic(__dirname + "/design-system/storybook-static"));
 
 var port = process.env.PORT || 5000;
 app.listen(port);
