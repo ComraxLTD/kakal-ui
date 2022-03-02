76a4a006e4bb527e3f94a738aa739784762e2dd6
dvirmon <dmenajem@gmail.com>
2022-03-02 08:55:06 +0200

    feat (typogrphy.comp)  - remove dir prop + click output

--- design-system/projects/kakal-ui/src/lib/typography/typography.component.ts
+++ design-system/projects/kakal-ui/src/lib/typography/typography.component.ts
@@ -7,28 +7,20 @@ import { Color, palette, Palette } from '../../styles/theme';
   styleUrls: ['./typography.component.scss'],
 })
 export class TypographyComponent implements OnInit {
-
-  public palette: Color = palette;
   @Input() public size: number;
   @Input() public weight: number;
-  @Input() public underline:boolean;
   @Input() public color: Palette;
-  @Input() public disabled: boolean
-  @Input() public dir: 'rtl' | 'ltr';
+  
+  @Input() public underline: boolean;
+  @Input() public disabled: boolean;
 
-  @Output() click: EventEmitter<void> = new EventEmitter()
+  public palette: Color = palette;
 
-  constructor(
-  ) { }
+  constructor() {}
 
   ngOnInit(): void {
     this.size = this.size || 1.4;
     this.weight = this.weight || 500;
     this.color = this.color || 'text';
-    this.dir = this.dir || 'rtl'
-  }
-
-  public onClick() {
-    this.click.emit()
   }
 }
