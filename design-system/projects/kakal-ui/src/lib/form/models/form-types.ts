867223b94c62756dbbdb7c392508aae70bfe5823
dvirmon <dmenajem@gmail.com>
2022-03-03 10:26:42 +0200

    feat (form-types) - remove un use lines

--- design-system/projects/kakal-ui/src/lib/form/models/form-types.ts
+++ design-system/projects/kakal-ui/src/lib/form/models/form-types.ts
@@ -3,19 +3,3 @@ import { FormOption as KKLFormOption } from './form-options';
 import { FormDataSource as KKLFormDataSource } from './form-datasource';
 
 export { FormEvents, KKLFormDataSource, KKLFormOption };
-
-// export enum FormEvents {
-//   DEFAULT = 'default',
-//   DISABLED = 'disable',
-//   CLEAR = 'clear',
-//   DELETE = 'delete',
-//   SAVE = 'save',
-//   EDIT = 'edit',
-//   ADD = 'add',
-//   CREATE = 'create',
-//   UPDATE = 'update',
-//   SUBMIT = 'submit',
-//   CHANGE = 'valueChange',
-//   SELECTED = 'optionSelected',
-//   MULTI_SELECTED = 'multiOptionSelected',
-// }
