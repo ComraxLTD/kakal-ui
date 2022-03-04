eefa1f31944e1086f0e557cae6cbeaf75469a735
dvirmon <dmenajem@gmail.com>
2022-03-04 13:46:42 +0200

    feat (table--action-cell)  - create tbale-action-cell comp

---
+++ design-system/projects/kakal-ui/src/lib/table/table-cells/table-action-cell/table-action-cell.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'pl-table-action-cell',
+  templateUrl: './table-action-cell.component.html',
+  styleUrls: ['./table-action-cell.component.scss']
+})
+export class TableActionCellComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
