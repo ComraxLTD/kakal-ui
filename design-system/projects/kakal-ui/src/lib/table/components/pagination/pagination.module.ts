c437d2c2e390a0f39f1d7090810e0e64158297db
dvirmon <dmenajem@gmail.com>
2022-03-06 19:18:37 +0200

    feat (tpackaje.json)  - update kkl-ui to 0.62

--- design-system/projects/kakal-ui/src/lib/table/components/pagination/pagination.module.ts
+++ design-system/projects/kakal-ui/src/lib/table/components/pagination/pagination.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 import { NgxPaginationModule } from 'ngx-pagination';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { PaginationComponent } from './pagination.component';
-import { UrlPaginationDirective } from './url-pagination.directive';
 import { KKLPaginationDirective } from './pagination.directive';
 
 @NgModule({
