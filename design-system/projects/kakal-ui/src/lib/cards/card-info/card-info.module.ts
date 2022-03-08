e7ae827ab2beb9cb29c3700e29844d34e6662406
Victor <victor@comrax.com>
2022-03-08 12:29:12 +0200

    fix build

--- design-system/projects/kakal-ui/src/lib/cards/card-info/card-info.module.ts
+++ design-system/projects/kakal-ui/src/lib/cards/card-info/card-info.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from "@angular/core";
 import { MatCardModule } from "@angular/material/card";
 import { KKLDirectivesModule } from "../../directives/directives.module";
 import { KKLTypographyModule } from "../../typography/typography.module";
+import { CardInfoComponent } from "./card-info.component";
 
 @NgModule({
     imports:[MatCardModule,KKLTypographyModule,KKLDirectivesModule],
-    declarations:[],
-    exports:[]
+    declarations:[CardInfoComponent],
+    exports:[CardInfoComponent]
 })
 
 export class KKLCardInfoModule {}
