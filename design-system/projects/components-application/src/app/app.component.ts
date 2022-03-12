b74606929165ad2ab929c94bb187c5e666e429d8
Victor Nikiforov <victor@comrax.com>
2022-03-12 10:46:47 +0200

    feat : navbar-bottom module

--- design-system/projects/components-application/src/app/app.component.ts
+++ design-system/projects/components-application/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, of } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -8,7 +9,9 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit {
   title = 'components-application';
   constructor() { }
+  showSave$:Observable<boolean>;
 
   ngOnInit(): void {
+    this.showSave$ = of(true)
   }
 }
