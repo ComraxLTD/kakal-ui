1c09269cb1e3f2af99396706a1b00a8058f9515d
dvirmon <dmenajem@gmail.com>
2022-03-13 13:30:47 +0200

    fix ()  - fix rangeNumber clear chnange

---
+++ design-system/projects/kakal-ui/src/lib/table/models/table.types.ts
@@ -0,0 +1,21 @@
+import { TableSelector } from './table.selectors';
+import {
+  TableState,
+  HeaderState,
+  FetchState,
+  SortState,
+  RowState,
+  ActionState,
+  PageState,
+} from './table.state';
+
+export {
+  TableSelector,
+  TableState,
+  FetchState,
+  SortState,
+  RowState,
+  HeaderState,
+  ActionState,
+  PageState,
+};
