6c6e7834b7dcb3ff187b91cee56ad85bfd51d1ac
dvirmon <dmenajem@gmail.com>
2022-03-15 18:25:11 +0200

    feat (kakl-ui.module)  - export and import KKLFiltersModule

--- design-system/projects/kakal-ui/src/lib/filters/filters.component.ts
+++ design-system/projects/kakal-ui/src/lib/filters/filters.component.ts
@@ -3,11 +3,12 @@ import { FilterState } from './filters.types';
 import { FiltersService } from './filters.service';
 
 @Component({
-  selector: 'kkl-table-filters',
+  selector: 'kkl-filters',
   templateUrl: './filters.component.html',
   styleUrls: ['./filters.component.scss'],
 })
 export class FiltersComponent implements OnInit {
+
   @Input() filtersState: FilterState;
 
   constructor(private filterService: FiltersService) {}
