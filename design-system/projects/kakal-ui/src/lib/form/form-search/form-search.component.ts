479bef555c5319198d94348b0f076267812c06cc
dvirmon <dmenajem@gmail.com>
2022-03-15 13:03:45 +0200

    feat (form-filter)  - fix advanced button style

--- design-system/projects/kakal-ui/src/lib/form/form-search/form-search.component.ts
+++ design-system/projects/kakal-ui/src/lib/form/form-search/form-search.component.ts
@@ -10,9 +10,16 @@ export class FormSearchComponent implements OnInit {
   @Input() public control: FormControl;
   @Input() public expended: boolean = true;
 
+  @Input() inRow: number = 3;
+
   constructor() {}
 
-  ngOnInit(): void {}
+  public inputs = [1, 2, 3, 4, 5, 5, 6, 6, 6, 7, 7, 8];
+  public flex: number;
+
+  ngOnInit(): void {
+    this.flex = 100 / this.inRow;
+  }
 
   public onClick() {
     this.expended = !this.expended;
