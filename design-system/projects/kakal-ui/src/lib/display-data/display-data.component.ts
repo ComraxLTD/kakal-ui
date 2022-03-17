bd66555c2227b4eafabbfac387b0e32999db6e24
Victor Nikiforov <victor@comrax.com>
2022-03-17 09:24:42 +0200

    working on display-data

--- design-system/projects/kakal-ui/src/lib/display-data/display-data.component.ts
+++ design-system/projects/kakal-ui/src/lib/display-data/display-data.component.ts
@@ -6,13 +6,15 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./display-data.component.scss']
 })
 export class DisplayDataComponent implements OnInit {
-  @Input() data: { label: string, value: string | number }[];
-  @Input() type: 'table' | 'default' | 'input';
+  @Input() data: { label: string, value: string | number, icon?: string }[]
+    | { label: string, value: string | number, icon?: string } = [];
+  @Input() type: 'table' | 'default' | 'input' = 'default';
 
   constructor() { }
-  fakeDATA = [{ label: 'test', value: 'test' }, { label: 'test', value: 'test' }];
+  fakeDATA = [{ label: 'test', value: 'test' }, { label: 'יתרה פיננסית :', value: '143,300' , icon:'tree' }];
   ngOnInit(): void {
     this.data = this.fakeDATA;
+    if (!Array.isArray(this.data)) this.data = [this.data];
     this.type = 'input';
   }
 
--- design-system/projects/kakal-ui/src/lib/display-data/display-data.module.ts
+++ design-system/projects/kakal-ui/src/lib/display-data/display-data.module.ts
@@ -3,9 +3,10 @@ import { DisplayDataComponent } from "./display-data.component";
 import { CommonModule } from "@angular/common";
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { KKLTypographyModule } from "../typography/typography.module";
+import { KKLIconModule } from "../icon/icon.module";
 
 @NgModule({
-    imports: [CommonModule, FlexLayoutModule,KKLTypographyModule],
+    imports: [CommonModule, FlexLayoutModule,KKLTypographyModule,KKLIconModule],
     declarations: [DisplayDataComponent],
     exports: [DisplayDataComponent]
 })
