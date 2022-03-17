5c2da5ec3fdba3578c0b832dfdb6525ff81f8bb6
dvirmon <dmenajem@gmail.com>
2022-03-17 13:10:55 +0200

    fix (form-range)  - implmemt value accesor interface

--- design-system/projects/kakal-ui/src/lib/form/form-range/form-range.component.ts
+++ design-system/projects/kakal-ui/src/lib/form/form-range/form-range.component.ts
@@ -1,21 +1,36 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import {
+  ControlValueAccessor,
+  FormControl,
+  FormGroup,
+  NG_VALUE_ACCESSOR,
+} from '@angular/forms';
 import { skip, take, filter, takeUntil, Subject } from 'rxjs';
 import { FilterRange } from '../../filters/filters.types';
 import { FormService, Question } from '../services/form.service';
+import { Range } from './question-range.model';
 
 @Component({
   selector: 'kkl-form-range',
   templateUrl: './form-range.component.html',
   styleUrls: ['./form-range.component.scss'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: FormRangeComponent,
+      multi: true,
+    },
+  ],
 })
-export class FormRangeComponent implements OnInit {
+export class FormRangeComponent implements OnInit, ControlValueAccessor {
   @Input() control: FormControl;
   @Input() questions: Question[];
 
   constructor(private formService: FormService) {}
 
   public formGroup: FormGroup;
+
+  private range: Range;
 
   private destroy: Subject<void>;
 
@@ -25,6 +40,35 @@ export class FormRangeComponent implements OnInit {
     this.destroy = new Subject();
     this.formGroup = this.setAmountGroup(this.questions);
   }
+  private _onChange: (v: Range) => void = (value: Range) => { };
+
+
+  // ControlValueAccessor interface methods
+  writeValue(value: Range) {
+    if (!value) {
+      this.rangeChange.emit(null);
+    }
+    this.range = value || null;
+    this._emitChangeEvent();
+  }
+
+  registerOnChange(fn: (v: Range | null) => void): void {
+    this._onChange = fn;
+  }
+
+  registerOnTouched(fn: Function) {}
+
+  public _onChangeEvent(event: Event) {
+    event.stopPropagation();
+
+    this._emitChangeEvent();
+  }
+
+  private _emitChangeEvent() {
+    this._onChange(this.range);
+    this.rangeChange.emit(this.range);
+  }
+
 
   public setAmountGroup(questions: Question[]): FormGroup {
     const group = this.formService.createQuestionGroup<FilterRange<number>>({
