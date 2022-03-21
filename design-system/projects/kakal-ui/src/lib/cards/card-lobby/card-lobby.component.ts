eb2bc3f83e93db69586b7dddb8842b19605d6e0e
dvirmon <dmenajem@gmail.com>
2022-03-21 14:18:29 +0200

    fix (card-lobby)  - remove un use unsubscribe

--- design-system/projects/kakal-ui/src/lib/cards/card-lobby/card-lobby.component.ts
+++ design-system/projects/kakal-ui/src/lib/cards/card-lobby/card-lobby.component.ts
@@ -7,39 +7,32 @@ import {
   Output,
 } from '@angular/core';
 import { CardLobbyModel } from './card-lobby.model';
-import { map, Observable, startWith, Subscription } from 'rxjs';
 import { BreakpointService } from '../../../services/breakpoint.service';
+import { map, Observable } from 'rxjs';
 
 @Component({
   selector: 'kkl-card-lobby',
   templateUrl: './card-lobby.component.html',
   styleUrls: ['./card-lobby.component.scss'],
 })
-export class CardLobbyComponent implements OnInit, OnDestroy {
+export class CardLobbyComponent implements OnInit {
   @Input() card: CardLobbyModel;
 
   public card$: Observable<CardLobbyModel>;
 
   @Output() click = new EventEmitter<CardLobbyModel>();
 
-  private subscription: Subscription;
   constructor(private breakpointService: BreakpointService) {}
 
   ngOnInit(): void {
     this.card$ = this.setSizeInMobile$();
   }
-
-  ngOnDestroy(): void {
-    this.subscription.unsubscribe();
-  }
-
   public onCardClick() {}
 
   private setSizeInMobile$(): Observable<CardLobbyModel> {
     return this.breakpointService.isMobile().pipe(
       map((md: boolean) => {
         const card = { ...this.card };
-        console.log(md);
         return {
           ...card,
           size: md ? 4 : card.size || 5.5,
