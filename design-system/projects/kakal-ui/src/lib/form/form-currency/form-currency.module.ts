811180ba4f7c20dcfe33bfbd080bd318a6c498d2
dvirmon <dmenajem@gmail.com>
2022-03-21 14:08:21 +0200

    fix (form-currecny)  - add formControl

--- design-system/projects/kakal-ui/src/lib/form/form-currency/form-currency.module.ts
+++ design-system/projects/kakal-ui/src/lib/form/form-currency/form-currency.module.ts
@@ -1,17 +1,18 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
 import { FormCurrencyComponent } from './form-currency.component';
 import { CurrencyService } from './form-currency.service';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { KKLFormInputModule } from '../form-input/form-input.module';
 import { KKLIconModule } from '../../icon/icon.module';
-import { MatSelectModule } from '@angular/material/select';
 import { KKLFormSelectModule } from '../form-select/form-select.module';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder } from '@angular/forms';
 
 @NgModule({
   imports: [
     CommonModule,
+    ReactiveFormsModule,
     FlexLayoutModule,
     KKLFormInputModule,
     KKLFormSelectModule,
