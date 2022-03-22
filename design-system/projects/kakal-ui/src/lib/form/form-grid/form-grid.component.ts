784cb729730e46409cbb3f0fab1e4ad87fd69639
dvirmon <dmenajem@gmail.com>
2022-03-22 09:28:05 +0200

    feat (form-grid) - add grid from comp

---
+++ design-system/projects/kakal-ui/src/lib/form/form-grid/form-grid.component.ts
@@ -0,0 +1,95 @@
+import { GridProps } from '../models/question.model';
+import { QuestionGroupModel } from '../models/question-group.model';
+import { FormGroup } from '@angular/forms';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  Input,
+  OnInit,
+  Output,
+  TemplateRef,
+} from '@angular/core';
+import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
+import { FormDataSource } from '../models/form-datasource';
+import { FormChangeEvent } from '../models/form.options';
+
+@Component({
+  selector: 'kkl-form-gird',
+  templateUrl: './form-grid.component.html',
+  styleUrls: ['./form-grid.component.scss'],
+})
+export class FormGridComponent implements OnInit {
+
+  @Input() public variant: 'flex' | 'grid' = 'grid';
+
+  @Input() public group: QuestionGroupModel;
+  @Input() public formDataSource: FormDataSource;
+
+  @Input() public rowHeight: number;
+  @Input() public gutter: number;
+
+  @Input() public slots: {
+    button?: TemplateRef<any>;
+    group?: ElementRef;
+  };
+
+  @Input() optionsSlot: { [key: string]: ElementRef };
+
+  public formGroup: FormGroup;
+  public grid: GridProps;
+  public hasButton: boolean = false;
+  public cols: string | number;
+
+  @Output() public submitEvent: EventEmitter<FormGroup> = new EventEmitter();
+
+  @Output() public selectChanged: EventEmitter<FormChangeEvent> =
+    new EventEmitter();
+  @Output() public optionSelected: EventEmitter<MatAutocompleteSelectedEvent> =
+    new EventEmitter();
+
+  @Output() public queryChanged: EventEmitter<FormChangeEvent> =
+    new EventEmitter();
+
+  @Output() fileChange = new EventEmitter<File[]>();
+
+  @Output() focusoutEvent: EventEmitter<string> = new EventEmitter();
+  @Output() focus: EventEmitter<FormChangeEvent> = new EventEmitter();
+
+  constructor() {}
+
+  ngOnInit() {
+    this.formGroup = this.group.formGroup;
+    this.grid = this.group.gridProps;
+    this.cols = this.group.gridProps?.cols || 1;
+    this.hasButton = this.group.hasButton || false;
+  }
+
+  public onSubmit() {
+    this.submitEvent.emit(this.formGroup);
+  }
+
+  public onSelectChanged(option: FormChangeEvent) {
+    this.selectChanged.emit(option);
+  }
+
+  public onQueryChanged(FormChangeEvent: FormChangeEvent): void {
+    this.queryChanged.emit(FormChangeEvent);
+  }
+
+  public onOptionSelected(event: MatAutocompleteSelectedEvent) {
+    this.optionSelected.emit(event);
+  }
+
+  public onFileChange(files: File[]) {
+    this.fileChange.emit(files);
+  }
+
+  public onFocusOut(key: string) {
+    this.focusoutEvent.emit(key);
+  }
+
+  public onFocus(FormChangeEvent: FormChangeEvent) {
+    this.focus.emit(FormChangeEvent);
+  }
+}
---
+++ design-system/projects/kakal-ui/src/lib/form/form-grid/form-grid.module.ts
@@ -0,0 +1,56 @@
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatSelectModule } from '@angular/material/select';
+import { KKLIconModule } from '../../icon/icon.module';
+import { KKLFormInputModule } from '../form-input/form-input.module';
+import { FormGridComponent } from './form-grid.component';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { KKLFormUploadModule } from '../form-upload/form-upload.module';
+import { KKLFormCurrencyModule } from '../form-currency/form-currency.module';
+import { KKLFormDateModule } from '../form-date/form-date.module';
+import { KKLFormSelectModule } from '../form-select/form-select.module';
+import { KKLFormAutoCompleteModule } from '../form-autocomplete/form-autocomplete.module';
+import { KKLFormRadioModule } from '../form-radio/form-radio.module';
+import { KKLFormTextEditorModule } from '../form-texteditor/form-texteditor.module';
+import { KKLFormCheckboxModule } from '../form-checkbox/form-checkbox.module';
+import { KKLFormCheckboxGroupModule } from '../form-checkbox-group/form-checkbox-group.module';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+  declarations: [FormGridComponent],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MatMenuModule,
+    MatListModule,
+    MatSelectModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatGridListModule,
+    MatInputModule,
+
+    FlexLayoutModule,
+
+    KKLIconModule,
+
+    // form components
+    KKLFormDateModule,
+    KKLFormInputModule,
+    KKLFormSelectModule,
+    KKLFormAutoCompleteModule,
+    KKLFormCurrencyModule,
+    KKLFormUploadModule,
+    KKLFormTextEditorModule,
+    KKLFormCheckboxModule,
+    KKLFormCheckboxGroupModule,
+    KKLFormRadioModule,
+  ],
+  exports: [FormGridComponent, KKLFormInputModule],
+})
+export class KKLFormModule {}
