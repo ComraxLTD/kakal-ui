db39cfbdd1700a420cfcd84cb5e480607875cef1
Victor <victor@comrax.com>
2022-03-23 16:11:54 +0200

    wokring on navbar menu

---
+++ design-system/projects/kakal-ui/src/lib/navbar-menu/navbar-menu.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from "@angular/core";
+import { NavbarMenuComponent } from "./navbar-menu.component";
+
+@NgModule({
+    imports:[],
+    declarations:[NavbarMenuComponent],
+    exports:[NavbarMenuComponent]
+})
+
+export class NavbarMenuModule {}
--- design-system/projects/kakal-ui/src/screens/layout/layout.module.ts
+++ design-system/projects/kakal-ui/src/screens/layout/layout.module.ts
@@ -5,9 +5,10 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { FlexLayoutModule } from "@angular/flex-layout";
 import { KKLNavbarModule } from '../../lib/navbar/navbar.module';
 import { KKLDirectivesModule } from '../../lib/directives/directives.module';
+import { NavbarMenuModule } from '../../lib/navbar-menu/navbar-menu.module';
 
 @NgModule({
-    imports: [CommonModule, MatSidenavModule, FlexLayoutModule
+    imports: [CommonModule, MatSidenavModule, FlexLayoutModule, NavbarMenuModule
         , KKLDirectivesModule, KKLNavbarModule],
     declarations: [LayoutComponent],
     exports: [LayoutComponent]
