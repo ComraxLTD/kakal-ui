d6f089df10e2d2da8826c84b934495031ece350f
dvirmon <dmenajem@gmail.com>
2022-03-25 11:34:59 +0300

    feat ()  - add bid comp from contracts

--- design-system/projects/components-application/src/app/app-routing.module.ts
+++ design-system/projects/components-application/src/app/app-routing.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { BidComponent } from './components/bid/bid.component';
 import { DetailsComponent } from './components/details/details.component';
 
 const routes: Routes = [
   { path: 'details', component: DetailsComponent },
   { path: 'details/:id', component: DetailsComponent },
-  { path: 'bid', component: BidComponent },
   {
     path: 'test', children: [
       { path: '', loadChildren: () => import('./test/test.module').then((m) => m.TestModule) }
