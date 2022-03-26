5816756f2836b342ac4273a5dd4b3d2d6fc06bf7
dvirmon <dmenajem@gmail.com>
2022-03-26 11:09:06 +0300

    feat (kkl-page)  - add style

--- design-system/projects/kakal-ui/src/lib/layouts/page/page.component.ts
+++ design-system/projects/kakal-ui/src/lib/layouts/page/page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'pl-page',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./page.component.scss']
 })
 export class PageComponent implements OnInit {
+
+  @Input() title : string
 
   constructor() { }
 
