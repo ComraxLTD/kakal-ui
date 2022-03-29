aae416b30f1aa729cc5d04597e28eed144f4fc4e
dvirmon <dmenajem@gmail.com>
2022-03-29 12:30:04 +0300

    feat (card-info) - add CommonModule import

--- design-system/projects/kakal-ui/src/lib/cards/card-info/card-info.component.ts
+++ design-system/projects/kakal-ui/src/lib/cards/card-info/card-info.component.ts
@@ -9,8 +9,6 @@ import { CardInfoModel } from './card-info.model';
 export class CardInfoComponent implements OnInit {
   @Input() public card: CardInfoModel;
 
-  public tooltip: CardInfoComponent;
-
   constructor() {}
 
   ngOnInit(): void {}
