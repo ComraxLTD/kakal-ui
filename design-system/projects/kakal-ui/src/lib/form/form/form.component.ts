4bce4fdf46c97ff085d4671536dcd1ce2a241332
dvirmon <dmenajem@gmail.com>
2022-03-30 15:17:36 +0300

    feat (form)  - update @Input with set and get

--- design-system/projects/kakal-ui/src/lib/form/form/form.component.ts
+++ design-system/projects/kakal-ui/src/lib/form/form/form.component.ts
@@ -11,6 +11,7 @@ import { FormGroup } from '@angular/forms';
 import { FormChangeEvent } from '../models/form.options';
 import { Question, OptionMap } from '../models/form.types';
 import { FormGrid } from '../models/question.types';
+import { FormService } from '../services/form.service';
 
 @Component({
   selector: 'kkl-form',
@@ -18,17 +19,49 @@ import { FormGrid } from '../models/question.types';
   styleUrls: ['./form.component.scss'],
 })
 export class FormComponent implements OnInit {
-  @Input() questions: Question[];
   @Input() formGroup: FormGroup;
-  @Input() grid: FormGrid;
-  @Input() optionsMap: OptionMap = {};
 
   @Input() rowHeight: number;
-
   @Input() optionsSlot: { [key: string]: ElementRef };
 
   @Input() buttonLabel: string = 'שמור';
   @Input() buttonTemp: TemplateRef<any>;
+
+  private _questions: Question[];
+
+  // handle form questions
+  @Input()
+  get questions() {
+    return this._questions;
+  }
+
+  set questions(value: Question[]) {
+    this._questions = value;
+  }
+
+  // handle grid ui
+  private _grid: FormGrid;
+
+  @Input()
+  get grid() {
+    return this._grid;
+  }
+
+  set grid(value: FormGrid) {
+    this._grid = value;
+  }
+
+  // handle optionMap of form
+  private _optionsMap: OptionMap = {};
+
+  @Input()
+  get optionsMap(): OptionMap {
+    return this._optionsMap;
+  }
+
+  set optionsMap(value: OptionMap) {
+    this._optionsMap = { ...value };
+  }
 
   variant: 'flex' | 'grid';
 
@@ -37,8 +70,16 @@ export class FormComponent implements OnInit {
   @Output() public formChanged: EventEmitter<FormChangeEvent> =
     new EventEmitter();
 
+  constructor(private formService: FormService) {}
+
   ngOnInit() {
-    this.variant = this.grid.variant || 'grid';
+    this.variant = this._grid.variant || 'grid';
+    // this.formGroup = this.formGroup || this.setFormGroup(this._questions);
+  }
+
+  private setFormGroup(questions) {
+    const group = this.formService.createQuestionGroup({ questions });
+    return group.formGroup;
   }
 
   public onSubmitEvent() {
