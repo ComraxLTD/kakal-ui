5d545d999930436b8b5e01645acbf6c30028ef35
dvirmon <dmenajem@gmail.com>
2022-04-03 16:46:41 +0300

    feat (layout) - fix cards ans input

--- design-system/projects/kakal-ui/src/lib/layouts/layout/layout.component.ts
+++ design-system/projects/kakal-ui/src/lib/layouts/layout/layout.component.ts
@@ -1,5 +1,4 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { MenuComponent } from '../../menu/menu.component';
 
 import { map, startWith } from 'rxjs/operators';
 import { Observable } from 'rxjs';
@@ -14,29 +13,11 @@ import { MenuCard } from '../../menu-bar/menu-card/menu-card.component';
 export class LayoutComponent implements OnInit {
 
   @Input() public showStatusPath: string[];
-  @Input() public menu: MenuComponent;
+  @Input() public cards : MenuCard[]
 
   public currentPath$: Observable<string>;
   public show$: Observable<boolean>;
   public mobile$: Observable<boolean>;
-
-  public cards : MenuCard[] = [
-    {
-      label: 'ועדות',
-      svgIcon: 'union',
-      path: 'committees',
-    },
-    {
-      label: 'ישיבות',
-      svgIcon: 'meetings',
-      path: 'meetings',
-    },
-    {
-      label: 'נושאים',
-      svgIcon: 'list',
-      path: 'subjects',
-    },
-  ]
 
   constructor(
     private routerService: RouterService,
