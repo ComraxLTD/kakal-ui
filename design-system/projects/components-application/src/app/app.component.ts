cbabf5d557e33a23ea74ef26a524c1882d89553c
Victor <victor@comrax.com>
2022-04-07 12:13:50 +0300

    fix carousel

--- design-system/projects/components-application/src/app/app.component.ts
+++ design-system/projects/components-application/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CardAddComponent } from '../../../kakal-ui/src/lib/cards/card-add/card-add.component';
 
 @Component({
   selector: 'app-root',
@@ -7,7 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
   constructor() { }
+  component = CardAddComponent;
+  cards = [
+    {title:'1',content:'test'},
+    {title:'2',content:'test'},
+    {title:'3',content:'test'},
+    {title:'4',content:'test'},
+    {title:'5',content:'test'},
+    {title:'6',content:'test'},
+    {title:'7',content:'test'},
+    {title:'8',content:'test'},
 
+  ]
   public card = {
     label: 'שם הכרטיס', // label inside card
     value: 2, // number inside card
