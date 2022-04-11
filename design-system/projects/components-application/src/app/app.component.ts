e6725cd29b29cab54b4af43c1e18ce02113c90b6
dvirmon <dmenajem@gmail.com>
2022-04-11 10:06:24 +0300

    feat (expand-panel) - form variant style

--- design-system/projects/components-application/src/app/app.component.ts
+++ design-system/projects/components-application/src/app/app.component.ts
@@ -3,50 +3,50 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Observable } from 'rxjs';
 import { CardAddComponent } from '../../../kakal-ui/src/lib/cards/card-add/card-add.component';
 
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
+  actions = [{ svgIcon: 'edit' }, { svgIcon: 'delete' }];
 
   eventClicked(event) {
     console.log(event);
   }
-  //calendar 
+  //calendar
   arr = [
     {
-      backgroundColor: "#F0F6FE",
-      start: "2022-04-10",
+      backgroundColor: '#F0F6FE',
+      start: '2022-04-10',
       // end: "2022-04-10T09:00",
       editable: true,
-      svg: "tree",
-      textColor: "black",
-      title: "מגרש ספורט",
-      type: "facility",
+      svg: 'tree',
+      textColor: 'black',
+      title: 'מגרש ספורט',
+      type: 'facility',
     },
     {
-      backgroundColor: "#F0F6FE",
-      start: "2022-04-10T08:00",
-      end: "2022-04-10T09:00",
+      backgroundColor: '#F0F6FE',
+      start: '2022-04-10T08:00',
+      end: '2022-04-10T09:00',
       editable: true,
-      svg: "tree",
-      textColor: "black",
-      title: "מגרש ספורט",
-      type: "facility"
+      svg: 'tree',
+      textColor: 'black',
+      title: 'מגרש ספורט',
+      type: 'facility',
     },
     {
-      backgroundColor: "#F0F6FE",
-      start: "2022-04-10T09:00",
-      end: "2022-04-10T10:00",
+      backgroundColor: '#F0F6FE',
+      start: '2022-04-10T09:00',
+      end: '2022-04-10T10:00',
       editable: true,
-      svg: "tree",
-      textColor: "black",
-      title: "מגרש ספורט",
-      type: "activity"
-    }
-  ]
+      svg: 'tree',
+      textColor: 'black',
+      title: 'מגרש ספורט',
+      type: 'activity',
+    },
+  ];
 
   //
   component = CardAddComponent;
@@ -59,21 +59,16 @@ export class AppComponent implements OnInit {
     { title: '6', content: 'test' },
     { title: '7', content: 'test' },
     { title: '8', content: 'test' },
+  ];
 
-  ]
-
-
-  ngOnInit(){
+  ngOnInit() {
     // setTimeout(() => {
     //   console.log(this.formgroup);
-      // this.editData = {number: 65657};
+    // this.editData = {number: 65657};
     //   this.questions = this.questions.concat([{
     //       key: 'password',
     //       controlType: 'password',
     //     }]);
     // }, 4000);
   }
-
-
-
 }
