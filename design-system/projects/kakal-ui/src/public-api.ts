a92efa2c1e4acedb23bd95ee838efc7d0a6b30d5
RamComrax <ram@comrax.com>
2022-04-11 12:46:57 +0300

    fixing export of paginator directive

--- design-system/projects/kakal-ui/src/public-api.ts
+++ design-system/projects/kakal-ui/src/public-api.ts
@@ -153,7 +153,7 @@ export * from './lib/kkl-table/pipes/table-group-cell.pipe';
 export * from './lib/kkl-table/table-actions.model';
 export * from './lib/kkl-table/table.model';
 export * from './lib/kkl-table/table-server.model';
-export * from './lib/kkl-table/directives/pagination-directive.directive';
+export * from './lib/kkl-table/directives/pagination.directive';
 export * from './lib/kkl-table/services/kkl-paginator.service';
 export * from './lib/kkl-table/kkl-table.module';
 
