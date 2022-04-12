a8623bf1cb768627083b4e30dde0d64ecb82e524
DvirMon <dmenajem@gmail.com>
2022-04-12 17:44:54 +0300

    feat (public.api)  - fix import table.model

--- design-system/projects/kakal-ui/src/public-api.ts
+++ design-system/projects/kakal-ui/src/public-api.ts
@@ -155,7 +155,7 @@ export * from './lib/kkl-table/pipes/table-group-cell.pipe';
 
 export * from './lib/kkl-table/models/table-server.model';
 export * from './lib/kkl-table/models/table-actions.model';
-export * from './lib/kkl-table/models/table-server.model';
+export * from './lib/kkl-table/models/table.model';
 
 export * from './lib/kkl-table/directives/pagination.directive';
 export * from './lib/kkl-table/services/kkl-paginator.service';
