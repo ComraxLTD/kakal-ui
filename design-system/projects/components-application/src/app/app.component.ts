6e6a3b1b01cec67e47b382578f3b4c62f31bcc7e
RamComrax <ram@comrax.com>
2022-04-12 17:05:56 +0300

    adjusting layout of table

--- design-system/projects/components-application/src/app/app.component.ts
+++ design-system/projects/components-application/src/app/app.component.ts
@@ -9,69 +9,41 @@ import { ControlBase, OptionsModel, RowActionModel, TableBase } from '../../../k
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  options:OptionsModel[] = [
-    {
-      //this key should be the same
-      key: 'firstQuestion',
-      val: [
-        { label: 'initial option1', value: 0 },
-        { label: 'initial option2', value: 1, },
-        { label: 'initial option3', value: 2,  },
-        { label: 'initial option4', value: 3,selected:true},
-      ],
-    },
-    {
-      //this key should be the same
-      key: 'secondQuestion',
-      val: [
-        { label: 'test1', value: 1,  },
-        { label: 'test2', value: 3, disabled: true },
-        { label: 'test3', value: 2, selected: true },
-      ],
-    },
-  ]
 
-  formGroup = new FormGroup({});
+  constructor() { }
 
-  questions: ControlBase[] = [
-    // {
-    //   key: 'first',
-    //   controlType: 'select',
-    //   options: 'firstQuestion',
-    //   multi: false,
-    //   label: 'בחירה ראשונה',
-    // },
-    // {
-    //   key: 'second',
-    //   controlType: 'select',
-    //   options: 'secondQuestion',
-    //   multi: false,
-    //   label: ' בחירה שניה',
-    //   // disabled: true
-    //   //,
-    // },
-    // {
-    //   key: 'autocomplete',
-    //   controlType: 'autocomplete',
-    //   options: 'firstQuestion',
-    //   multi: true,
-    //   label: 'local autocomplete',
-    //   // disabled: true
-    //   //,
-    // },
-    {
-      key: 'currency',
-      controlType: 'currency'
-    }
-  ];
+  ngOnInit() {
+    this.data = [{ key: 'First', label: 'בדיקה' }, { key: 'Second', label: 'test' }, { key: 'Third', label: 'עמוד 3' }];
+  }
 
   dataSource: any[] = [
     {
-      committeeId: 'wtwrt',
-      remiTikimCount: 'werwsfwe',
-
-    }
+      committeeId: '1',
+      remiTikimCount: 'remiTikimCount',
+      committeeDate: 'committeeDate'
+    },
+    {
+      committeeId: '2',
+      remiTikimCount: 'remiTikimCount',
+      committeeDate: 'committeeDate'
+    },
+    {
+      committeeId: '3',
+      remiTikimCount: 'remiTikimCount',
+      committeeDate: 'committeeDate'
+    },
+    {
+      committeeId: '4',
+      remiTikimCount: 'remiTikimCount',
+      committeeDate: 'committeeDate'
+    },
   ]
+
+    columns: TableBase[] = [
+      { key: 'committeeId', label: 'Id', controlType: 'number',},
+      { key: 'remiTikimCount', label: 'remiTikimCount', controlType: 'text', button: {type: 'inlineExpand', icon: 'expand'}},
+      { key: 'committeeDate', label: 'תאריך', controlType: 'text', },
+    ];
 
   rowActions: RowActionModel[] = [
     {
@@ -91,65 +63,19 @@ export class AppComponent {
     },
   ]
 
+  data!: { key: string, label: string }[];
 
-  columns: TableBase[] = [
-    { key: 'committeeId', label: 'Id', controlType: 'number',},
-    { key: 'remiTikimCount', label: 'remiTikimCount', controlType: 'number', button: {type: 'inlineExpand', icon: 'expand'}},
-    { key: 'committeeDate', label: 'תאריך', controlType: 'date', },
-  ];
-
-  editData =  'ert'
-  //{
-    //select: { label: 'editData', value: 88 }
-  //}
-
-  constructor() { }
-
-  ngOnInit() {
-    setTimeout(() => {
-      console.log(this.formGroup);
-      this.questions = this.questions.concat([{
-        key: 'time',
-        controlType: 'time'
-      }])
-      // this.editData = { number: 65657 };
-    }, 4000);
-  }
 
   onQueryChanged(event:any) {
     console.log(event);
   }
 
-  onSelectChanged(event:any) {
-    console.log(event);
-
-    if (event.key === 'first') {
-      this.options = [
-        {
-          //this key should be the same
-          key: 'firstQuestion',
-          val: [
-            { label: 'server option1', value: 0 },
-            { label: 'server option2', value: 2,  selected: true},
-            { label: 'server option3', value: 3, },
-          ],
-        },
-        {
-          //this key should be the same
-          key: 'secondQuestion',
-          val: [
-            { label: 'test1', value: 1 },
-            { label: 'test2', value: 3, selected: true },
-            { label: 'test3', value: 2},
-          ],
-        },
-      ];
-    }
-  }
-
   onOpenChanged(event:any) {
     console.log(event);
+  }
 
+  onExpand(event: any) {
+    console.log(event);
   }
 
 }
