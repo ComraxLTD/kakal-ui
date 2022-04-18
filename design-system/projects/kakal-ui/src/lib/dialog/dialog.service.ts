4cec2bded23fcba0fe8619a0af6846bc7e558b3d
Alex Shvetsov <alex.s@comrax.com>
2022-04-18 13:59:42 +0300

    dialog added

--- design-system/projects/kakal-ui/src/lib/dialog/dialog.service.ts
+++ design-system/projects/kakal-ui/src/lib/dialog/dialog.service.ts
@@ -5,6 +5,7 @@ import {
   MatDialogRef,
   MAT_DIALOG_DATA,
 } from '@angular/material/dialog';
+import { ComponentRef } from 'react';
 import { Observable } from 'rxjs';
 import { DialogAlertComponent } from '../dialog-alert/dialog-alert.component';
 import { DialogComponent } from './dialog.component';
@@ -17,111 +18,18 @@ export interface DialogData {
   providedIn: 'root',
 })
 export class DialogService {
-  public readonly DISPLAY_MSG_PREFIX = 'thisMessageIsForDisplay';
-  public readonly ERROR_MSG = 'מצטערים, קרתה תקלה ולא ניתן לבצע את הפעולה';
 
   constructor(private dialog: MatDialog) {}
 
-  public open(
-    dialogRef?: TemplateRef<any>,
-    config?: MatDialogConfig
-  ): MatDialogRef<DialogComponent> {
-    return this.dialog.open(dialogRef || DialogComponent, config);
+  openDialogGetAfterClosedObs(data:{component:ComponentRef<any>,closeBtnLabel?:string,saveBtnLabel?:string}):Observable<any>{
+ return   this.dialog.open(DialogComponent, {
+      data: { component: data.component, closeBtnLabel:data.closeBtnLabel,saveBtnLabel:data.saveBtnLabel }
+    }).afterClosed()
   }
-
-  private openDefault(
-    config?: MatDialogConfig
-  ): MatDialogRef<DialogAlertComponent> {
-    const alertConfig = {
-      ...config,
-    };
-
-    return this.dialog.open(DialogAlertComponent, alertConfig);
-  }
-
-  public openConfirm(options: {
-    message: string;
-    isConfirm?: boolean;
-  }): Observable<boolean> {
-    const dialogRef: MatDialogRef<DialogAlertComponent> = this.openAlert({
-      ...options,
-    });
-    return dialogRef.afterClosed();
-  }
-
-  public openAlert(options: {
-    message: string;
-    title?: string;
-    isConfirm?: boolean;
-  }) {
-    const { message, title, isConfirm } = options;
-    return this.openDefault({
-      data: {
-        title,
-        message,
-        buttonText: isConfirm
-          ? {
-              confirm: 'כן',
-              cancel: 'לא',
-            }
-          : { confirm: 'אישור' },
-      },
-    });
-  }
-
-  public openError(options: {
-    message: string;
-    title?: string;
-    config?: MatDialogConfig;
-  }): MatDialogRef<DialogAlertComponent> {
-    const { config, message, title } = options;
-
-    const configError = {
-      ...config,
-      panelClass: 'kkl-alert-dialog',
-      data: {
-        title: title || 'הפעולה לא הושלמה',
-        message:
-          message?.indexOf(this.DISPLAY_MSG_PREFIX) >= 0
-            ? message?.replace(this.DISPLAY_MSG_PREFIX, '')
-            : message || this.ERROR_MSG,
-      },
-    };
-
-    return this.dialog.open(DialogAlertComponent, configError);
-  }
-
-  public openSuccess(options: {
-    message: string;
-    title?: string;
-    config?: MatDialogConfig;
-  }): MatDialogRef<DialogAlertComponent> {
-    const { config, message, title } = options;
-    const configError = {
-      ...config,
-      panelClass: 'kkl-alert-dialog',
-      data: {
-        title: title || 'הפעולה הושלמה בהצלחה!',
-        message,
-      },
-    };
-
-    return this.dialog.open(DialogAlertComponent, configError);
-  }
-
-  public openMessage(options: {
-    message: string;
-    config?: MatDialogConfig;
-  }): MatDialogRef<DialogAlertComponent> {
-    const { config, message } = options;
-    const configError = {
-      ...config,
-      data: {
-        title: 'הפעולה הושלמה בהצלחה!',
-        message,
-      },
-    };
-
-    return this.dialog.open(DialogAlertComponent, configError);
-  }
+  openDialog(data:{component:ComponentRef<any>,closeBtnLabel?:string,saveBtnLabel?:string}):void{
+       this.dialog.open(DialogComponent, {
+         data: { component: data.component, closeBtnLabel:data.closeBtnLabel,saveBtnLabel:data.saveBtnLabel }
+       })
+     }
+    get
 }
