e9cd15da497468044f5ffca3242a5bfe667beae0
DvirMon <dmenajem@gmail.com>
2022-04-23 16:14:16 +0300

    feat (grups) - add gropus folder

---
+++ design-system/projects/kakal-ui/src/lib/groups/step-group/step-group.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'pl-step-group',
+  templateUrl: './step-group.component.html',
+  styleUrls: ['./step-group.component.scss']
+})
+export class StepGroupComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ design-system/projects/kakal-ui/src/lib/groups/step-group/step-group.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { StepGroupComponent } from './step-group.component';
+
+
+
+@NgModule({
+  declarations: [
+    StepGroupComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class StepGroupModule { }
