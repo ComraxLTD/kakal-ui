62ae337560a1f5e22aec3f231f63263f93cce6bf
DvirMon <dmenajem@gmail.com>
2022-04-24 21:15:27 +0300

    feat (circle-group) - working on divider style

--- design-system/projects/kakal-ui/src/lib/groups/circle-group/circle-group.module.ts
+++ design-system/projects/kakal-ui/src/lib/groups/circle-group/circle-group.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CircleGroupComponent } from './circle-group.component';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatDividerModule } from '@angular/material/divider';
 
 @NgModule({
   declarations: [CircleGroupComponent],
-  imports: [CommonModule, FlexLayoutModule],
+  imports: [CommonModule, FlexLayoutModule, MatDividerModule],
   exports: [CircleGroupComponent],
 })
 export class KKLCircleGroupModule {}
