50a39c5a0333b10d6272291c5ff949855718a57d
dvirmon <dmenajem@gmail.com>
2022-04-25 09:23:13 +0300

    fix (circle-group) - step seelct works with emitter

--- design-system/projects/kakal-ui/src/lib/groups/circle-group/circle-group.module.ts
+++ design-system/projects/kakal-ui/src/lib/groups/circle-group/circle-group.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { CircleGroupComponent } from './circle-group.component';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatDividerModule } from '@angular/material/divider';
 
+import {KKLButtonModule} from '../../button/button.module'
+
+import { CircleGroupComponent } from './circle-group.component';
 @NgModule({
   declarations: [CircleGroupComponent],
-  imports: [CommonModule, FlexLayoutModule, MatDividerModule],
+  imports: [CommonModule, FlexLayoutModule, MatDividerModule, KKLButtonModule],
   exports: [CircleGroupComponent],
 })
 export class KKLCircleGroupModule {}
