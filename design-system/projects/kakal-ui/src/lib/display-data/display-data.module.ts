80d1486bb3c1a652d5dda4c65d860bc9a5880777
DvirMon <dmenajem@gmail.com>
2022-04-29 17:18:44 +0300

    feat (display-data) - add display-data comp

---
+++ design-system/projects/kakal-ui/src/lib/display-data/display-data.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DisplayDataComponent } from './display-data.component';
+
+
+
+@NgModule({
+  declarations: [
+    DisplayDataComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class DisplayDataModule { }
