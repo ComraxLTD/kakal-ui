42915c3f0f369142726049392d53bcd6d2598d51
dvirmon <dmenajem@gmail.com>
2022-05-02 09:47:49 +0300

    feat (status-group) - update syntex

--- design-system/projects/kakal-ui/src/lib/groups/status-group/status-group.component.ts
+++ design-system/projects/kakal-ui/src/lib/groups/status-group/status-group.component.ts
@@ -3,11 +3,11 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { CardStatus } from '../../cards/card-status/card-status.component';
 import { CardOptions } from '../../cards/card.model';
 
-export interface StepsSelectionEvent {
+export interface StatusSelectionEvent {
   selectedIndex: number;
 
   /** The step instance now selected. */
-  selectedStep: CardStatus;
+  selectedStatus: CardStatus;
 
   source?: CardStatus[];
 }
@@ -17,23 +17,21 @@ export interface StepsSelectionEvent {
   styleUrls: ['./status-group.component.scss'],
 })
 export class StatusGroupComponent implements OnInit {
-
-  @Input() steps: CardStatus[];
+  @Input() status: CardStatus[];
   @Input() direction: 'row' | 'column' = 'row';
   @Input() options: CardOptions;
 
-  @Output() statusSelection = new EventEmitter<StepsSelectionEvent>();
+  @Output() statusSelection = new EventEmitter<StatusSelectionEvent>();
 
   constructor() {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
-  onStepStatusSelect(step: CardStatus, index: number) {
-    const event: StepsSelectionEvent = {
-      selectedStep: step,
+  onStepStatusSelect(status: CardStatus, index: number) {
+    const event: StatusSelectionEvent = {
+      selectedStatus: status,
       selectedIndex: index,
-      source: this.steps,
+      source: this.status,
     };
     this.statusSelection.emit(event);
   }
