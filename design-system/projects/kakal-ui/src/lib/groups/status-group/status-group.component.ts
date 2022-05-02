26083a4fa69d7c7ac31c7c528b4aa6185b23b63d
dvirmon <dmenajem@gmail.com>
2022-05-02 08:35:51 +0300

    fix (statys-group) - add status-group comp from comrax-dev

---
+++ design-system/projects/kakal-ui/src/lib/groups/status-group/status-group.component.ts
@@ -0,0 +1,40 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+
+import { CardStatus } from '../../cards/card-status/card-status.component';
+import { CardOptions } from '../../cards/card.model';
+
+export interface StepsSelectionEvent {
+  selectedIndex: number;
+
+  /** The step instance now selected. */
+  selectedStep: CardStatus;
+
+  source?: CardStatus[];
+}
+@Component({
+  selector: 'kkl-status-group',
+  templateUrl: './status-group.component.html',
+  styleUrls: ['./status-group.component.scss'],
+})
+export class StatusGroupComponent implements OnInit {
+
+  @Input() steps: CardStatus[];
+  @Input() direction: 'row' | 'column' = 'row';
+  @Input() options: CardOptions;
+
+  @Output() statusSelection = new EventEmitter<StepsSelectionEvent>();
+
+  constructor() {}
+
+  ngOnInit(): void {
+  }
+
+  onStepStatusSelect(step: CardStatus, index: number) {
+    const event: StepsSelectionEvent = {
+      selectedStep: step,
+      selectedIndex: index,
+      source: this.steps,
+    };
+    this.statusSelection.emit(event);
+  }
+}
