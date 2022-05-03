52ee434862b4cb7c923d5a68b00264b2dbeb95ab
alex-shvetsov-comrax <alex.s@comrax.com>
2022-05-03 15:49:19 +0300

    card css fix, dialog css,fix

--- design-system/projects/kakal-ui/src/lib/cards/card-add/card-add.module.ts
+++ design-system/projects/kakal-ui/src/lib/cards/card-add/card-add.module.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { FlexLayoutModule } from "@angular/flex-layout";
+import { MatTooltipModule } from "@angular/material/tooltip";
 import { KKLIconModule } from "../../icon/icon.module";
 import { KKLTypographyModule } from "../../typography/typography.module";
 import { CardAddComponent } from "./card-add.component";
 
 @NgModule({
-    imports: [CommonModule, FlexLayoutModule, KKLIconModule, KKLTypographyModule],
+    imports: [CommonModule, FlexLayoutModule, KKLIconModule, KKLTypographyModule,MatTooltipModule],
     declarations: [CardAddComponent],
     exports: [CardAddComponent]
 })
