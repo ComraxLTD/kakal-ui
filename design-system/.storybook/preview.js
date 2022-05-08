8377a0d445037b313415720aa9895bba33c9e716
Victor <victor@comrax.com>
2022-05-08 15:25:25 +0300

    removed indigo pink

--- design-system/.storybook/preview.js
+++ design-system/.storybook/preview.js
@@ -1,6 +1,5 @@
 
 import '!style-loader!css-loader!sass-loader!./../projects/kakal-ui/src/styles/global.scss';
-import '!style-loader!css-loader!sass-loader!./../node_modules/@angular/material/prebuilt-themes/indigo-pink.css';
 import { setCompodocJson } from "@storybook/addon-docs/angular";
 import docJson from "../documentation.json";
 setCompodocJson(docJson);
