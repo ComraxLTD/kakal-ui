4024a94c76f4542ef133a40535ad16b4c1db2307
RamComrax <ram@comrax.com>
2022-05-11 16:58:44 +0300

    removing unused code

--- design-system/projects/components-application/src/app/app.component.ts
+++ design-system/projects/components-application/src/app/app.component.ts
@@ -1,20 +1,11 @@
 import { Component } from '@angular/core';
 import {
   CardStatus,
-  DialogAlertComponent,
-  DialogService,
   MenuCard,
   PageHeadlineService,
   RouterService,
-  RowActionEvent,
-  RowActionModel,
-  RowExpandEvent,
   StatusSelectionEvent,
-  TableBase,
 } from '../../../kakal-ui/src/public-api';
-import { MatDialogRef } from '@angular/material/dialog';
-import { CustomDialogComponent } from '../../../kakal-ui/src/lib/custom-dialog/custom-dialog.component';
-import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -22,7 +13,6 @@ import { FormGroup } from '@angular/forms';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-
   title = 'education';
 
   cards: MenuCard[] = [
@@ -55,7 +45,7 @@ export class AppComponent {
 
   constructor(
     private routerService: RouterService,
-    private pageHeadlineSource: PageHeadlineService,
+    private pageHeadlineSource: PageHeadlineService
   ) {}
 
   ngOnInit(): void {}
