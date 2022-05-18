bd751020a968201acfc705d4439b248fbd3f5f41
DvirMon <dmenajem@gmail.com>
2022-05-18 12:59:38 +0300

    fix (kkl-lobby-grid) - fix ui on mobile

--- design-system/projects/kakal-ui/src/lib/grids/lobby-grid/lobby.component.ts
+++ design-system/projects/kakal-ui/src/lib/grids/lobby-grid/lobby.component.ts
@@ -3,36 +3,51 @@ import { BreakpointService } from '../../../services/breakpoint.service';
 import { CardLobbyModel } from '../../cards/card-lobby/card-lobby.component';
 import { map, Observable } from 'rxjs';
 
+interface LobbyGrid {
+  cols: number;
+  rows: number;
+  size: number;
+}
+
 @Component({
   selector: 'kkl-lobby-grid',
   templateUrl: './lobby.component.html',
   styleUrls: ['./lobby.component.scss'],
 })
 export class LobbyComponent implements OnInit {
-  @Input() public cols: number;
-  @Input() public rows: number;
-  @Input() public moduleTitle: string;
-  @Input() public cards: CardLobbyModel[];
+  @Input() cols: number;
+  @Input() rows: number;
+  @Input() moduleTitle: string;
+  @Input() cards: CardLobbyModel[];
 
-  public md$: Observable<{value:boolean}>;
+  ui$: Observable<LobbyGrid>;
+
+  md$: Observable<{ value: boolean }>;
 
   @Output() cardClick: EventEmitter<string> = new EventEmitter<string>();
 
   constructor(private breakpointService: BreakpointService) {}
 
   ngOnInit(): void {
-    this.cols = Math.min(this.cols, Math.floor(window.innerWidth/180));
+    this.cols = Math.min(this.cols, Math.floor(window.innerWidth / 180));
+    console.log(this.cols);
     this.rows = this.rows || 2;
-    this.md$ = this.mapIsMobile();
+    this.ui$ = this.mapIsMobile();
   }
 
-  mapIsMobile() {
+  mapIsMobile(): Observable<LobbyGrid> {
     return this.breakpointService.isMobile().pipe(
-      map(val => { return { value: val } }),
+      map((mobile) => {
+        return {
+          cols: this.cols,
+          rows: mobile ? Math.ceil(this.cards.length / 2) : this.rows,
+          size: mobile ? 16 : 18,
+        };
+      })
     );
   }
 
-  public onCardClick(card) {
+  onCardClick(card) {
     this.cardClick.emit(card.path);
   }
 }
