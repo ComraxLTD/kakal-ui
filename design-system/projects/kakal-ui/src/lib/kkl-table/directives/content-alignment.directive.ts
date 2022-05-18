584d77f6faa22c5c632d8c2b7d502ec61351341e
RamComrax <ram@comrax.com>
2022-05-18 15:06:20 +0300

    improving alignment directive
    deleting comments.

--- design-system/projects/kakal-ui/src/lib/kkl-table/directives/content-alignment.directive.ts
+++ design-system/projects/kakal-ui/src/lib/kkl-table/directives/content-alignment.directive.ts
@@ -9,19 +9,27 @@ import { Directive } from '@angular/core';
 export class ContentAlignmentDirective implements OnInit {
 
   @Input() buttonJustifyContent: {isMobile: boolean, isNumber: boolean};
-  @Input() justifyContent: {isMobile: boolean};
+  @Input() justifyContent: {isMobile: boolean, isNumber: boolean};
 
   @HostBinding('style.justify-content') public justifyContentValue:string
 
   ngOnInit(): void {
     if(this.buttonJustifyContent){
+      this.justifyContentValue = this.buttonAlignment() + ' !important'
+    }
+    if(this.justifyContent){
       this.justifyContentValue = this.alignment() + ' !important'
     }
   }
 
-  alignment(){
+  buttonAlignment(){
     if(this.buttonJustifyContent.isMobile) return 'space-between'
     if(this.buttonJustifyContent.isNumber) return 'center'
+    return 'start'
+  }
+
+  alignment(){
+    if(!this.justifyContent.isNumber) return 'start'
     if(!this.justifyContent.isMobile) return 'center'
     return 'start'
   }
