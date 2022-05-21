834b50b43ea38cd448c5b6b107833eb7ca5281c8
DvirMon <dmenajem@gmail.com>
2022-05-21 18:58:15 +0300

    feat (customer-details) - test

--- design-system/projects/components-application/src/app/modules/new-reservation/modules/details/components/costumer-details/costumer-details.component.ts
+++ design-system/projects/components-application/src/app/modules/new-reservation/modules/details/components/costumer-details/costumer-details.component.ts
@@ -26,7 +26,12 @@ export class CostumerDetailsComponent implements OnInit {
     // { key: 'costumerType', label: 'סוג לקוח', controlType: 'autocomplete' },
     // { key: 'contactName', label: 'שם איש קשר', controlType: 'autocomplete' },
     // { key: 'cellPhone', label: 'נייד', controlType: 'autocomplete' },
-    { key: 'email', label: 'דוא"ל', controlType: 'text' },
+    {
+      key: 'email',
+      label: 'דוא"ל',
+      controlType: 'text',
+      button: { type: 'inlineExpand', label: 'פרטים', button: true },
+    },
     { key: 'address', label: 'כתובת', controlType: 'toggle' },
     { key: 'payingCostumer', label: 'לקוח משלם', controlType: 'select' },
   ];
