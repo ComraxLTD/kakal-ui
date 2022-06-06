8d975a6dd4433faed65814517b21c5db6eed6c47
israelComrax <101564668+israelComrax@users.noreply.github.com>
2022-06-06 15:51:39 +0700

    add toggle

--- design-system/projects/components-application/src/app/app.component.ts
+++ design-system/projects/components-application/src/app/app.component.ts
@@ -491,9 +491,9 @@ export class AppComponent {
     // in the first column, there is an additional 'button' property, which makes the cells in that column into buttons
     // the 'button' property correlates with the RowActionModel, accept for the 'label', which has no effect on the UI
     { key: 'id', label: 'Id', controlType: 'number', button: { type: 'inlineNavigation' } },
-    { key: 'name', label: 'Name', controlType: 'text', notEditable: true},
+    { key: 'name', label: 'Name', controlType: 'toggle'},
     // in yearsOfExperience column, we can see the 'colIcon' which is used for adding an icon to all cells in that column
-    { key: 'yearsOfExperience', label: 'YearsOfExperience', controlType: 'number', colIcon: 'tree', cellTemplate: 'yearsOfExperienceTemplateName' },
+    { key: 'yearsOfExperience', label: 'YearsOfExperience', controlType: 'number', colIcon: 'tree',  },
     { key: 'occupation', label: 'Occupation', controlType: 'text', button: { type: 'visibility', icon: 'add' } },
     { key: 'city', label: 'עיר', controlType: 'select' },
     // { key: 'dob', label: 'תאריך', controlType: 'date' },
