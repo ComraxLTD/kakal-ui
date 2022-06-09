d2e7ab7b6c3b0e1f1198ff0074b41280ed76bc05
dvirmon <dmenajem@gmail.com>
2022-06-09 11:10:33 +0300

    feat (kkl-ngx-form) - pass column as TableBase

--- design-system/projects/kakal-ui/src/lib/kkl-ngx-table/components/table-form/table-form.component.ts
+++ design-system/projects/kakal-ui/src/lib/kkl-ngx-table/components/table-form/table-form.component.ts
@@ -1,31 +1,40 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { QuestionBase } from '../../../form/models/question.model';
+import { TableBase } from '../../../kkl-table/models/table.model';
 
 @Component({
   selector: 'kkl-ngx-table-form',
   templateUrl: './table-form.component.html',
-  styleUrls : ['./table-form.component.scss']
+  styleUrls: ['./table-form.component.scss'],
 })
-export class NgxTableFormComponent implements OnInit  {
-  @Input() question!: QuestionBase;
+export class NgxTableFormComponent implements OnInit {
+  @Input() column!: TableBase;
   @Input() initial!: any;
-
+  
+  question!: QuestionBase;
   control: FormControl | FormGroup;
 
   @Output() rowEdited = new EventEmitter<any>();
 
-  constructor() {
-  }
+  constructor() {}
   ngOnInit(): void {
+    this.question = { ...this.column } as QuestionBase;
+
     const data = this.getCellData(this.question.key);
     switch (this.question.controlType) {
       case 'dateRange':
       case 'range':
-        this.control = new FormGroup({start: new FormControl(data?.start), end: new FormControl(data?.end)});
+        this.control = new FormGroup({
+          start: new FormControl(data?.start),
+          end: new FormControl(data?.end),
+        });
         break;
       case 'currency':
-        this.control = new FormGroup({sum: new FormControl(data?.sum), currency: new FormControl(data?.currency)});
+        this.control = new FormGroup({
+          sum: new FormControl(data?.sum),
+          currency: new FormControl(data?.currency),
+        });
         break;
       // case 'costum':
       //   break;
@@ -36,14 +45,12 @@ export class NgxTableFormComponent implements OnInit  {
   }
 
   getCellData(key: string) {
-    if(this.initial) {
+    if (this.initial) {
       return this.initial[key];
     } else {
       return null;
     }
   }
-
-
 
   // onQueryChanged(event, control: ControlBase) {
   //   this.rowEdited.emit();
@@ -69,6 +76,4 @@ export class NgxTableFormComponent implements OnInit  {
   //     control.focusChanged(event.value);
   //   }
   // }
-
-
 }
