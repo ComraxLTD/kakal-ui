bab328b553fe657819e373c543cc9796714b038b
dvirmon <dmenajem@gmail.com>
2022-06-09 13:51:30 +0300

    feat button.module) - update stroke.directive folder

--- design-system/projects/kakal-ui/src/lib/button/button.module.ts
+++ design-system/projects/kakal-ui/src/lib/button/button.module.ts
@@ -9,12 +9,13 @@ import { KKLIconModule } from '../icon/icon.module';
 import { KKLTypographyModule } from '../typography/typography.module';
 
 import { ButtonComponent } from './components/button/button.component';
-import { StrokeButtonComponent } from './components/stroke-button/stroke-button.component';
 import { AddButtonComponent } from './components/add-button/add-button.component';
 import { SortButtonComponent } from './components/sort-button/sort-button.component';
 
+import { StrokeButtonComponent } from './components/stroke-button/stroke-button.component';
+import { KKLStrokedButtonDirective } from './components/stroke-button/stroked-button.directive';
+
 import { KKLButtonDirective } from './directives/button.directive';
-import { KKLStrokedButtonDirective } from './directives/stroked-button.directive';
 import { KKLDirectivesModule } from '../directives/directives.module';
 import { KKLActionButtonDirective } from './directives/action-button.directive';
 import { KKLFormButtonDirective } from './directives/form-button.directive';
