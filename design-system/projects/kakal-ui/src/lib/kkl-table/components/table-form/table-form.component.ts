c3966d60a50f64f2a846b0a440fea9c20d67a91e
dvirmon <dmenajem@gmail.com>
2022-06-09 11:05:09 +0300

    feat (kkl-form) - add typw to event

--- design-system/projects/kakal-ui/src/lib/kkl-table/components/table-form/table-form.component.ts
+++ design-system/projects/kakal-ui/src/lib/kkl-table/components/table-form/table-form.component.ts
@@ -1,55 +1,56 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { ControlContainer } from '@angular/forms';
+import { FormChangeEvent } from '../../../form/models/form.options';
 import { QuestionBase } from '../../../form/models/question.model';
 import { ControlBase } from '../../../mei-form/models/control.model';
-import { KklFormActions } from '../../../mei-form/models/kkl-form-events';
+import {
+  KklFormActions,
+  KklFormChangeEvent,
+} from '../../../mei-form/models/kkl-form-events';
 
 @Component({
   selector: 'kkl-table-form',
   templateUrl: './table-form.component.html',
 })
-export class TableFormComponent implements OnInit  {
+export class TableFormComponent implements OnInit {
   @Input() public question!: QuestionBase;
 
   @Output() rowEdited = new EventEmitter<void>();
 
-  constructor(public controlContainer: ControlContainer) {
-  }
-  ngOnInit(): void {
-  }
+  constructor(public controlContainer: ControlContainer) {}
+  ngOnInit(): void {}
 
-
-
-  onQueryChanged(event, control: ControlBase) {
+  onQueryChanged(event: KklFormChangeEvent, control: ControlBase) {
     this.rowEdited.emit();
-    if(control.queryChanged) {
-      control.queryChanged({value: event.value, query: event.query});
+    if (control.queryChanged) {
+      control.queryChanged({ value: event.value, query: event.query });
     }
   }
-  onSelectChanged(event, control: ControlBase) {
+  onSelectChanged(event: FormChangeEvent, control: ControlBase) {
     this.rowEdited.emit();
-    if(control.selectChanged) {
+    if (control.selectChanged) {
       control.selectChanged(event.value);
     }
   }
-  onOpenedChange(event, control: ControlBase) {
+  onOpenedChange(event: KklFormChangeEvent, control: ControlBase) {
     this.rowEdited.emit();
-    if(control.openedChange) {
-      control.openedChange({value: event.value, opened: event.action === KklFormActions.OPENED_SELECT});
+    if (control.openedChange) {
+      control.openedChange({
+        value: event.value,
+        opened: event.action === KklFormActions.OPENED_SELECT,
+      });
     }
   }
-  onValueChanged(event, control: ControlBase) {
+  onValueChanged(event: KklFormChangeEvent, control: ControlBase) {
     this.rowEdited.emit();
-    if(control.valueChanged) {
+    if (control.valueChanged) {
       control.valueChanged(event.value);
     }
   }
-  onFocusChanged(event, control: ControlBase) {
+  onFocusChanged(event: KklFormChangeEvent, control: ControlBase) {
     this.rowEdited.emit();
-    if(control.focusChanged) {
+    if (control.focusChanged) {
       control.focusChanged(event.value);
     }
   }
-
-
 }
