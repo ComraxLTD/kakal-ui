1eefdc963bb7aa5233757d6101fb6521c12aa98f
dvirmon <dmenajem@gmail.com>
2022-06-12 14:47:23 +0300

    feat (table-cell.pipe) - add typing

--- design-system/projects/kakal-ui/src/lib/mei-services/pipes/table-cell-pipe.pipe.ts
+++ design-system/projects/kakal-ui/src/lib/mei-services/pipes/table-cell-pipe.pipe.ts
@@ -1,55 +1,78 @@
-import { CurrencyPipe, DatePipe, formatDate } from '@angular/common';
-import { Inject, LOCALE_ID, Pipe, PipeTransform } from '@angular/core';
+import { CurrencyPipe, DatePipe } from '@angular/common';
+import { Pipe, PipeTransform } from '@angular/core';
+import { DateRange } from '@fullcalendar/angular';
+import { RadioOption } from '../../form/form-radio/question-radio.model';
+import { Range } from '../../form/form-range/question-range.model';
+import { SelectOption } from '../../form/form-select/question-select.model';
 @Pipe({
-  name: 'tableCellPipe'
+  name: 'tableCellPipe',
 })
 export class TableCellPipe implements PipeTransform {
+  constructor() {}
 
-    constructor() {
-    }
+  transform(
+    element:
+      | string
+      | SelectOption
+      | RadioOption
+      | DateRange
+      | { sum: number; currency: string },
+    type: string
+  ) {
+    switch (type) {
+      // case 'input':
+      // case 'text':
+      // case 'password':
+      // case 'number':
+      // case 'textarea':
+      // case 'sum':
+      // case 'email':
+      // case 'phone':
+      // case 'time':
+      // case 'texteditor':
+      // case 'upload':
+      //   return element;
+      case 'select':
+        const select = element as SelectOption;
+        return select.label;
 
-    transform(element: any, type: any) {
-        switch (type) {
-            // case 'input':
-            // case 'text':
-            // case 'password':
-            // case 'number':
-            // case 'textarea':
-            // case 'sum':
-            // case 'email':
-            // case 'phone':
-            // case 'time':
-            // case 'texteditor':
-            // case 'upload':
-            //   return element;
-            case 'select':
-            case 'autocomplete':
-            case 'radio':
-              if(Array.isArray(element)) {
-                return element.map((a: any) => a.label);
-              }
-              return element.label;
-            case 'date':
-              return new DatePipe('he-HE').transform(element);
-            case 'dateRange':
-              return new DatePipe('he-HE').transform(element.start) +' - '+ new DatePipe('he-HE').transform(element.end);
-            case 'currency':
-              return new CurrencyPipe('he-HE').transform(element.sum, element.currency, 'symbol', '1.0-3');
-            case 'range':
-              return element.start +' - '+ element.end;
-            case 'toggle':
-            case 'checkbox':
-              if(element) {
-                return '\u2713';
-              }
-              return '';
-            // case 'upload':
-            default:
-              return element;
+      case 'autocomplete':
+        const autocomplete = element as SelectOption;
+        return autocomplete.label;
+      case 'radio':
+        if (Array.isArray(element)) {
+          return element.map((a: RadioOption) => a.label);
         }
+        const radio = element as RadioOption;
+        return radio.label;
+      case 'date':
+        return new DatePipe('he-HE').transform(element.toString());
+      case 'dateRange':
+        const dateRange = element as DateRange;
+        return (
+          new DatePipe('he-HE').transform(dateRange.start) +
+          ' - ' +
+          new DatePipe('he-HE').transform(dateRange.end)
+        );
+      case 'currency':
+        const currency = element as { sum: number; currency: string };
+        return new CurrencyPipe('he-HE').transform(
+          currency.sum,
+          currency.currency,
+          'symbol',
+          '1.0-3'
+        );
+      case 'range':
+        const range = element as Range;
+        return range.start + ' - ' + range.end;
+      case 'checkbox':
+        if (element) {
+          return '\u2713';
+        }
+        return 'upload';
+      case 'upload':
+      default:
+        return element;
     }
-
-
-
-
+  }
 }
